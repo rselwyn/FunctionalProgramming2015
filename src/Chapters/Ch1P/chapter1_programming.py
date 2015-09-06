35323bf3e38b05341d225b8cfba04f3d826677ef
Robert Selwyn <robertselwyn@roberts-mbp.attlocal.net>
2015-09-05 22:07:07 -0700

    src added

---
+++ src/Chapters/Ch1P/chapter1_programming.py
@@ -0,0 +1,25 @@
+>>> print("hello, world!")
+hello, world!
+>>> print("hello", "world")
+hello world
+>>> print(3)
+3
+>>> print(3.0)
+3.0
+>>> print(2+3)
+5
+>>> print(2.0+3.0)
+5.0
+>>> print("2" + "3")
+23
+>>> print("2 + 3 =", 2+3)
+2 + 3 = 5
+>>> print(2*3)
+6
+>>> print(2**3)
+8
+>>> print(2/3)
+0.6666666666666666
+>>> print (2.0/3.0)
+0.6666666666666666
+>>> 
