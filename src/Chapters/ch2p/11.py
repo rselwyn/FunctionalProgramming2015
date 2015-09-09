84775dd0d15aaa48db806a718f8bf056e8b57cf2
Robbie S <robbie@selwynsoftware.com>
2015-09-08 18:51:01 -0700

    question change to problem 11

--- src/Chapters/ch2p/11.py
+++ src/Chapters/ch2p/11.py
@@ -1,9 +1,7 @@
-
-def convert():
-    pi = 3.1415926535
-    print("This program converts a radius to the area of a circle")
-    radius = float(input("Please enter the radius"))
-    print("The area of a circle with radius", radius,"is", pi*radius**2)
-    
- 
-    
+def findval():
+    #iterate 100 times
+    for i in range(100):
+        #first, I take in an input from a user
+        #then I evaluate the expression given
+        #then I print it, it is that simple
+       print(eval(input("Input an expression to be calculated")))
