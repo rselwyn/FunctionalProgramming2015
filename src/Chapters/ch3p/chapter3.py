15328011bf9aa7d6464ca0f9267328b932119538
Robbie S <robbie@selwynsoftware.com>
2015-09-11 13:07:45 -0700

    initial part of ch3p

--- src/Chapters/ch3p/chapter3.py
+++ src/Chapters/ch3p/chapter3.py
@@ -1,3 +1,6 @@
+#do 1,4 6-11, ec(12-17)
+import math
+import os
 def problem1():
     #define pi
     pi = 3.14159265358979323846264338279502
@@ -9,13 +12,53 @@ def problem1():
     #volume
     vol = (4/3)*pi*rad**3
     print("The volume is", vol, "and the surface area is", sa)
-def problem2():
-    pi = 3.14159265358979323846264338279502
-    #calculate cost per square inch of pizza
-    rad = float(input("diameter"))/2
-    cost = float(input("cost"))
-    print("The cost per square inch is", (pi *rad**2)/cost)
-def problem3():
+def problem4():
+    #this program prints distance to a lightning strike based on time elapsed
+    li = input("Time elapsed between lightening flash and thunder")
+    print("You are", 1100*li, "ft")
+def problem6():
+    x1,y1 = input("Enter coordinate 1 sperated by commas")
+    x2,y2 = input("enter coordinate 2 seperated by commas")
+    print("the slope is", (y2-y1)/(x2-x1))
+def problem7():
+    x1,y1 = input("Enter coordinate 1 sperated by commas")
+    x2,y2 = input("enter coordinate 2 seperated by commas")
+    d = ((y2-y1)**2 + (x2-x1)**2)**(1/2)
+    print("The distance between the coordinates is",d)
+def problem8():
+    C = int(input("year"))//100
+    year = C*100
+    epact = (8 +(C//4) - C + ((8*C+13)//25) + 11* (year%19))%30
+    print(epact)
+#WRITE PROBLEM 10
+def problem10():
+    x = float(input("angle measure"))
+    h = float(input("enter the height of the house"))
+    radians = math.pi/180 * x
+    length = h/(math.sin(radians))
+    print(length)
+def problem11():
+    x = int(input("how many natural numbers should printed"))
+    y = 0
+    for i in range(x):
+        
+        y = y+ i
+    print(y)
     
-    
-    
+def problem12():
+    x = int(input("how many natural numbers should printed"))
+    y = 0
+    for i in range(x):
+        
+        y = y+ i**3
+    print(y)
+def problem13():
+    #this program sums numbers given by a user
+    nums = int(input("how many numbers is your series"))
+    x = 0
+    print("please enter each number in your series and press enter")
+    for i in range(nums):
+        x = x + float(input())
+    print("the total value of your series of numbers is",x)    
+                 
+                  
