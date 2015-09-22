12fed20f07283cd28cf40c708253b2957019b975
Robbie S <robbie@selwynsoftware.com>
2015-09-21 21:20:58 -0700

    Working on problem 3, half way done

--- src/Chapters/ch4p/chapter4.py
+++ src/Chapters/ch4p/chapter4.py
@@ -1,4 +1,6 @@
 import graphics
+#2,3,5,6,9
+#ec 7, 8, 10, 11
 def test():
     #this isnt a problem
     win = graphics.GraphWin()
@@ -35,3 +37,25 @@ def problem2():
     yellow.setOutline("Black")
     yellow.setFill("yellow")
     yellow.draw(win)
+def problem3():
+    #draw a face
+    win = graphics.GraphWin("Archery", 400,400)
+
+    yellow = graphics.Circle(graphics.Point(200,200),80)
+    yellow.setOutline("Black")
+    yellow.setFill("yellow")
+    yellow.draw(win)
+
+    
+    blue = graphics.Circle(graphics.Point(175, 175),20)
+    blue.setOutline("Black")
+    blue.setFill("Blue")
+    blue.draw(win)
+
+    blue2 = graphics.Circle(graphics.Point(225, 175),20)
+    blue2.setOutline("Black")
+    blue2.setFill("Blue")
+    blue2.draw(win)
+
+#    line = graphics.Line(graphics.Point(100,2), graphics.Point(3,2))
+#    line.draw(win)
