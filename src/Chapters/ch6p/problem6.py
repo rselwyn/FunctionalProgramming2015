6c8719ff5c34cab6f0126ff128044d9d92b1f4f9
Robbie S <robbie@selwynsoftware.com>
2015-10-12 12:16:49 -0700

    Added problem 4,6,7

---
+++ src/Chapters/ch6p/problem6.py
@@ -0,0 +1,19 @@
+#WRITE A FUNCTION THAT COMPUTES THE AREA OF A TRIANGLE GIVEN THE LENGTH
+#OF ITS THREE SIDES AS PARAMETERS
+
+
+
+a = float(input("enter the value of side 1"))
+b = float(input("enter the value of side 2"))
+c = float(input("enter the value of side 3"))
+
+def problem9(a,b,c):
+    #get side lengths value
+    #calculate the value of (a+b+c)/2
+    s = (a+b+c)/2
+    #use the formula provided in the book to find the area
+    area = (s*(s-a)*(s-b)*(s-c))**(1/2)
+    print("the area is", area)
+    
+
+problem9(a,b,c)
