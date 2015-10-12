8eef5f58016cd7967e5d7d91809060346a7b0a5b
Robbie S <robbie@selwynsoftware.com>
2015-10-11 21:57:57 -0700

    FINISHED PROBLEM 4

--- src/Chapters/ch6p/chapter6.py
+++ src/Chapters/ch6p/chapter6.py
@@ -1,1 +1,22 @@
 #chapter 6 programmng assignments
+#4,6,7,9-13
+#bonus 3,9,14-17
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Chapters/ch6p/problem4.py
@@ -0,0 +1,37 @@
+#WRITE A PROGRAM THAT SUMS THE FIRST n NATURAL NUMBERS
+#WRITE A PROGRAM THAT SUMS THE CUBE OF THE FIRST n NATURAL NUMBERS
+
+def sumN(x):
+    #prints natural numbers
+    #x is the number of natural numbers
+    y = 0
+    #y is the value where the numbers get accumulated
+    for i in range(x+1):
+        #assuming that the set of natural numbers is all integers
+        #(0, infinity) in interval notation
+        #add one to nuber of iterations because natural numbers don't include zero
+        #add the current number the loop is on to the total
+        y = y+ i
+    #print the total
+    print(y)
+
+def sumNCubes(x):
+    #x is the number of natural numbers)
+    y = 0
+    #y is the accumulator
+    for i in range(x+1):
+        #assuming that the set of natural numbers is (0, infinity) in interval notation
+        #add one because natural numbers dont include zero
+        
+        #add the cube of the current number to the total
+        y = y+ i**3
+    #print the accumulated value
+    print(y)
+
+#INVOKE THE FUNCTIONS
+
+sumN(int(input("ENTER THE NUMBER OF NATURAL NUMBERS TO BE ADDED")))
+
+sumNCubes(int(input("ENTER THE NUMBER OF NATURAL NUMBERS TO BE CUBED AND THEN ADDED")))
+
+
