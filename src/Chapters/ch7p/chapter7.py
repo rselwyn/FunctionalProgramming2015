ba6430e5ded6c3e7f300544301b7233a1299350e
Robbie S <robbie@selwynsoftware.com>
2015-10-28 12:29:04 -0700

    problem2

--- src/Chapters/ch7p/chapter7.py
+++ src/Chapters/ch7p/chapter7.py
@@ -8,3 +8,19 @@ def problem1():
         print("You made {0:.2f} this week".format(hours*wage))
     else:
         print("You made {0:.2f} this week".format(40*wage + (1.5*(hours-40))*wage ))
+        
+def problem2():
+    x = float(input("Enter the quiz score"))
+    #get the input of the quiz score as a number
+
+    #print it out as the corresponding letter grade
+    if x == 5:
+        print("A")
+    elif x == 4:
+        print("B")
+    elif x == 3:
+        print("C")
+    elif x ==2:
+        print("D")
+    else:
+        print("F")   
