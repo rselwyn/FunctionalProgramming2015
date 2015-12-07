7b6cf5d29cbeefe95001507503c7bd8a175698d0
Robbie S <robbie@selwynsoftware.com>
2015-12-07 12:26:36 -0800

    problem5

--- src/Chapters/ch11p/chapter11.py
+++ src/Chapters/ch11p/chapter11.py
@@ -1,1 +1,15 @@
 #Ch11
+#########5-8, 10, and 11
+
+def problem5():
+    liste = [0,3,10,2]
+    liste = reverse(liste)
+    print(liste)
+    
+def reverse(myList):
+    newList = []
+    lend = len(myList)-1
+    while lend>=0:
+        newList.append(myList[lend])
+        lend = lend-1
+    return newList
