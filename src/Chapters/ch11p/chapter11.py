7f443917c6e62c52e6762f39782e2ee67eee66bf
Robbie S <robbie@selwynsoftware.com>
2015-12-07 12:52:33 -0800

    added the rest of proble 5

--- src/Chapters/ch11p/chapter11.py
+++ src/Chapters/ch11p/chapter11.py
@@ -2,9 +2,9 @@
 #########5-8, 10, and 11
 
 def problem5():
-    liste = [0,3,10,2]
-    liste = reverse(liste)
-    print(liste)
+    print("The functions are reverse(myList), count(myList, numberToCOunt)")
+    return
+    
     
 def reverse(myList):
     newList = []
@@ -13,3 +13,40 @@ def reverse(myList):
         newList.append(myList[lend])
         lend = lend-1
     return newList
+
+def count(myList, num):
+    count = 0
+    for i in myList:
+        if num == int(i):
+            count+=1
+    return count
+
+def index(myList, lookFor):
+    for i in range(len(myList)):
+        if myList[i] == lookFor:
+            return i
+def sort():
+    myList = [3,25,7,10]
+    while not inOrder(myList):
+        myList = exchange(myList)
+    print(myList)
+    
+def exchange(myList):
+    elem = len(myList)-1
+    for i in range(elem):
+        if (myList[i] > myList[i+1]):
+            myList[i],myList[i+1] = myList[i+1],myList[i]
+    return myList
+
+def inOrder(myList):
+    for i in range(len(myList)-1):
+        if (myList[i] < myList[i+1]):
+            continue
+        else:
+            return False
+    return True
+def isin(myList,lookFor):
+    for i in myList:
+        if i == lookFor:
+            return True
+    return False
